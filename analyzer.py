22256bb227207188c4d7b8af70a8a381bb36d982
nquirogac <nquirogac@unal.edu.co>
2024-03-06 10:52:48 -0500

    fixing special characters

--- analyzer.py
+++ analyzer.py
@@ -2,11 +2,11 @@ import re
 
 keyWords = "acadena|alogico|anumero|leer|limpiar|caso|cierto|verdadero|defecto|otro|desde|elegir|error|escribir|imprimir|poner|falso|fin|funcion|fun|hasta|imprimirf|mientras|nulo|osi|repetir|retorno|retornar|ret|romper|si|sino|tipo|rango|si|sino|fun|funcion|para|en"
 
-operators = {"(&&)": "and", "\|\|": "or", "\.\.": "concat", "(\.)": "period", "(\,)": "comma", "(;)": "semicolon","(:)": "colon", "(\{)": "opening_key", "(\})": "closing_key", "(\[)": "opening_bra", "(\])": "closing_bra", "\(": "opening_par", "\)": "closing_par", "(\++)": "increment", "(--)": "decrement", "(%=)": "mod_assign", "(/=)": "div_assign", "(\*=)": "times_assign", "(-=)": "minus_assign", "(\+=)": "plus_assign", "(\+)": "plus", "(-)": "minus", "(\*)": "times", "(/)": "div", "(\^)": "power", "(%)": "mod", "(<=)": "leq", "(>=)": "geq", "(==)": "equal", "(!=)": "neq", "(<)": "less", "(>)": "greater", "(=)": "assign", "(!)": "not", "(~=)": "regex"}
+operators = {"&&": "and", "\|\|": "or", "\.\.": "concat", "\.": "period", "\,": "comma", ";": "semicolon",":": "colon", "\{": "opening_key", "\}": "closing_key", "\[": "opening_bra", "\]": "closing_bra", "\(": "opening_par", "\)": "closing_par", "(\+\+)": "increment", "\-\-": "decrement", "%=": "mod_assign", "/=": "div_assign", "\*=": "times_assign", "-=": "minus_assign", "\+=": "plus_assign", "\+": "plus", "-": "minus", "\*": "times", "/": "div", "\^": "power", "%": "mod", "<=": "leq", ">=": "geq", "==": "equal", "!=": "neq", "<": "less", ">": "greater", "=": "assign", "!": "not", "~=": "regex"}
 operatorsKeys = "|".join(operators.keys())
 
 idsRegex = "^[a-zA-Z_][a-zA-Z0-9_]*" 
-commentsRegex = "^(//).|#"
+commentsRegex = "//|#"
 multiLineCommentsRegex = "^(/*)."
 
 
@@ -26,17 +26,28 @@ def maximalToken(line, flag, j):
                 return True
     else:
         return False
-def defineOperators(line, flag, j):
-    #print(line[flag:j], flag, j, len(line))
+
+def findOperators(line, flag, j):
     for key in operators:
-        if j+2 < len(line) and (re.match(key, line[flag:j+2])):
-            """ print(key)
-            print("doble",line[flag:j+2]) """
-            return False
-        elif(re.match(key, line[flag:j+1])):
+        if(re.match(key, line[flag:j+1])):
             print("<tkn_"+operators[key]+","+str(i+1)+","+str(flag+1)+">")
-            return True
-            
+              
+def defineOperators(line, flag, j):
+    #print(1,line[flag:j+1], flag, j, len(line))
+    if (j+1 <= len(line) and ((line[j+1] == " ") or (line[j+1] == "\n") or (j+1 == len(line)))):              #if the word is a space
+        findOperators(line, flag, j)
+        return True
+    elif (re.match(commentsRegex, line[flag:j+2])): 
+        print("comentario")  
+        return False  
+    elif  re.match("\w+", line[j+1]):
+        findOperators(line, flag, j) 
+        return True  
+    elif (re.fullmatch(operatorsKeys, line[flag:j+2])==None):
+        findOperators(line, flag, j) 
+        return True 
+    else:
+        return False       
 
 for i in range(len(lines)):
     flag = 0

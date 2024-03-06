5223fff414e1842611d6d339e6013f65d10c1c08
nquirogac <nquirogac@unal.edu.co>
2024-03-06 08:09:46 -0500

    caso 2 fix

--- analyzer.py
+++ analyzer.py
@@ -2,7 +2,7 @@ import re
 
 keyWords = "acadena|alogico|anumero|leer|limpiar|caso|cierto|verdadero|defecto|otro|desde|elegir|error|escribir|imprimir|poner|falso|fin|funcion|fun|hasta|imprimirf|mientras|nulo|osi|repetir|retorno|retornar|ret|romper|si|sino|tipo|rango|si|sino|fun|funcion|para|en"
 
-operators = {"(&&)": "and", "\|\|": "or", "\.\.": "concat", "(\.)": "period", "(\,)": "comma", "(;)": "semicolon","(:)": "colon", "(\{)": "opening_key", "(\})": "closing_key", "(\[)": "opening_bra", "(\])": "closing_bra", "(\()": "opening_par", "(\))": "closing_par", "(\++)": "increment", "(--)": "decrement", "(%=)": "mod_assign", "(/=)": "div_assign", "(\*=)": "times_assign", "(-=)": "minus_assign", "(\+=)": "plus_assign", "(\+)": "plus", "(-)": "minus", "(\*)": "times", "(/)": "div", "(\^)": "power", "(%)": "mod", "(<=)": "leq", "(>=)": "geq", "(==)": "equal", "(!=)": "neq", "(<)": "less", "(>)": "greater", "(=)": "assign", "(!)": "not", "(~=)": "regex"}
+operators = {"(&&)": "and", "\|\|": "or", "\.\.": "concat", "(\.)": "period", "(\,)": "comma", "(;)": "semicolon","(:)": "colon", "(\{)": "opening_key", "(\})": "closing_key", "(\[)": "opening_bra", "(\])": "closing_bra", "\(": "opening_par", "\)": "closing_par", "(\++)": "increment", "(--)": "decrement", "(%=)": "mod_assign", "(/=)": "div_assign", "(\*=)": "times_assign", "(-=)": "minus_assign", "(\+=)": "plus_assign", "(\+)": "plus", "(-)": "minus", "(\*)": "times", "(/)": "div", "(\^)": "power", "(%)": "mod", "(<=)": "leq", "(>=)": "geq", "(==)": "equal", "(!=)": "neq", "(<)": "less", "(>)": "greater", "(=)": "assign", "(!)": "not", "(~=)": "regex"}
 operatorsKeys = "|".join(operators.keys())
 
 idsRegex = "^[a-zA-Z_][a-zA-Z0-9_]*" 
@@ -12,24 +12,28 @@ lines = file.readlines()
 file.close()
 
 def maximalToken(line, flag, j):
-    if (j+2 == len(line)):
-        return True
-    elif (re.match(operatorsKeys, line[j+1])):
+    if (j+1 == len(line)):
         return True
     elif (j+1 < len(line) ):              #if the word is a space
             if (line[j+1] == " "): 
-                return True 
+                return True
+            elif (line[j+1] == "\n"): 
+                return True     
+            elif (re.match(operatorsKeys, line[j+1])):
+                return True
     else:
         return False
 def defineOperators(line, flag, j):
-    #print(line[flag:j], flag, j, len(line))
+    print(line[flag:j], flag, j, len(line))
     for key in operators:
-        if (re.match(key, line[flag:j])):
+        if j+2 < len(line) and (re.match(key, line[flag:j+2])):
+            """ print(key)
+            print("doble",line[flag:j+2]) """
+            return False
+        elif(re.match(key, line[flag:j+1])):
             print("<tkn_"+operators[key]+","+str(i+1)+","+str(flag+1)+">")
             return True
-        elif(re.match(key, line[flag:j+1])):
-            print("doble")
-            return False
+            
 
 for i in range(len(lines)):
     flag = 0
@@ -49,15 +53,16 @@ for i in range(len(lines)):
             else:
                 continue  
         
-        elif(re.match(operatorsKeys, line[flag:j])):            #if the word is an operator or special character
+        elif(re.match(operatorsKeys, line[flag:j+1])):            #if the word is an operator or special character
             #print("ddd")
             if(defineOperators(line, flag, j)):
-                flag = j
+                flag = j+1
             else:
                 continue 
         
         elif (re.match(idsRegex, line[flag:j+1])):              #if the word is an identifier
             if (maximalToken(line, flag, j)):
+                #print("dfs",line[flag:j+1], flag, j, len(line))
                 print("<id,"+line[flag:j+1]+","+str(i+1)+","+str(flag+1)+">")
                 flag = j+1
             else:
